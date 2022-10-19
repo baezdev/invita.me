06754f80c3f5f38f0f68ad81e981057ea1bb4710
baezdev <baezdev@gmail.com>
2022-10-19 00:08:08 -0500

    feat🤠: validacion

--- js/forms.js
+++ js/forms.js
@@ -16,38 +16,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const form = document.querySelector("form");
 const formInputs = document.querySelectorAll(".form__input-container");
+const values = {};
+const camposValidados = {};
 
 const validateForm = ({ target }) => {
-  if (validator.isEmpty(target.value)) {
-    showError(target, "El campo no puede estar vacío");
+  if (!validator.isEmpty(target.value)) {
+    values[target.name] = target.value;
+
+    switch (target.name) {
+      case "email":
+        if (!validator.isEmail(target.value)) {
+          showError(target, "Eso no parece un correo electrónico");
+          camposValidados[target.name] = false
+        } else {
+          deleteError(target);
+          camposValidados[target.name] = true
+        }
+        break;
+      case "password":
+        if (!validator.isStrongPassword(target.value, { minSymbols: 0 })) {
+          showError(target, "Al menos 8 caracteres, una minúscula y un número");
+          camposValidados[target.name] = false
+        } else {
+          deleteError(target);
+          camposValidados[target.name] = true
+        }
+        break;
+      case "name":
+        if (!validator.isAlpha(target.value, "es-ES", { ignore: "-'s" })) {
+          showError(target, "Eso no parece un nombre");
+          camposValidados[target.name] = false
+        } else {
+          deleteError(target);
+          camposValidados[target.name] = true
+        }
+        break;
+    }
   } else {
-    deleteError(target);
-  }
-
-  switch (target.name) {
-    case "email":
-      if (!validator.isEmail(target.value)) {
-        showError(target, "Eso no parece un correo electrónico");
-      } else {
-        deleteError(target);
-      }
-      break;
-    case "password":
-      if (!validator.isStrongPassword(target.value, { minSymbols: 0 })) {
-        showError(target, "Al menos 8 caracteres, una minúscula y un número");
-      } else {
-        deleteError(target);
-      }
-      break;
-
-    default:
-      break;
+    showError(target, "El campo no puede estar vacío");
   }
 };
 
 const showError = (element, message) => {
   const formInput = element.parentElement;
   const formInputContainer = element.parentElement.parentElement;
+  formInput.classList.remove("form__input-success");
   formInput.classList.add("form__input-error");
   formInputContainer.children[1].innerText = message;
 };
@@ -56,10 +69,16 @@ const deleteError = (element) => {
   const formInput = element.parentElement;
   const formInputContainer = element.parentElement.parentElement;
   formInput.classList.remove("form__input-error");
+  formInput.classList.add("form__input-success");
   formInputContainer.children[1].innerText = "";
 };
 
 formInputs.forEach((input) => {
   input.addEventListener("blur", validateForm);
   input.addEventListener("keyup", validateForm);
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(camposValidados);
 });
