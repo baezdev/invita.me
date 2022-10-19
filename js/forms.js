b309b7fe1d359c19a6bbea37625f74c8f19ef2a7
baezdev <baezdev@gmail.com>
2022-10-18 19:28:36 -0500

    feat🤠: validacion del formulario

--- js/forms.js
+++ js/forms.js
@@ -1,3 +1,5 @@
+import validator from "validator";
+// ---
 const btnShowPass = document.querySelector(".show__pass");
 const passwordInput = document.querySelector("[name=password]");
 
@@ -14,14 +16,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const form = document.querySelector("form");
 const formInputs = document.querySelectorAll(".form__input-container");
-console.log(formInputs);
 
-const validarCampo = (e) => {
-  console.log("cambio");
+const validateForm = ({ target }) => {
+  if (validator.isEmpty(target.value)) {
+    showError(target, "El campo no puede estar vacío");
+  } else {
+    deleteError(target);
+  }
+
+  switch (target.name) {
+    case "email":
+      if (!validator.isEmail(target.value)) {
+        showError(target, "Eso no parece un correo electrónico");
+      } else {
+        deleteError(target);
+      }
+      break;
+    case "password":
+      if (!validator.isStrongPassword(target.value, { minSymbols: 0 })) {
+        showError(target, "Al menos 8 caracteres, una minúscula y un número");
+      } else {
+        deleteError(target);
+      }
+      break;
+
+    default:
+      break;
+  }
+};
+
+const showError = (element, message) => {
+  const formInput = element.parentElement;
+  const formInputContainer = element.parentElement.parentElement;
+  formInput.classList.add("form__input-error");
+  formInputContainer.children[1].innerText = message;
+};
+
+const deleteError = (element) => {
+  const formInput = element.parentElement;
+  const formInputContainer = element.parentElement.parentElement;
+  formInput.classList.remove("form__input-error");
+  formInputContainer.children[1].innerText = "";
 };
 
 formInputs.forEach((input) => {
-  /* input.addEventListener("blur", validarCampo);
-  input.addEventListener("keyup", validarCampo); */
-  console.log(input);
+  input.addEventListener("blur", validateForm);
+  input.addEventListener("keyup", validateForm);
 });
