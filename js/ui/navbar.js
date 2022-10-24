05a9eb6336cd5dfd843effb4420058edb0aa74ea
baezdev <baezdev@gmail.com>
2022-10-23 22:49:37 -0500

    feat🤠:  boton de cerrar sesion

--- js/ui/navbar.js
+++ js/ui/navbar.js
@@ -1,3 +1,5 @@
+import { supabase } from "../config/supabase.config";
+
 export const showMenu = () => {
   const buttonMenu = document.querySelector(".button__menu");
   const navbarMenu = document.querySelector(".navbar__menu");
@@ -14,13 +16,9 @@ export const showMenu = () => {
   });
 };
 
-export const generateLinks = () => {
+export const generateLinks = async () => {
   const navbarMenuContainer = document.querySelector(".navbar__menu-container");
   const links = [
-    {
-      text: "acceder",
-      url: "login.html",
-    },
     {
       text: "contacto",
       url: "contact.html",
@@ -42,6 +40,37 @@ export const generateLinks = () => {
   const location = window.location.pathname;
   const isPage = location.includes("pages");
 
+  //Creacion del boton de cerrar sesion o de acceder
+  const authUser = await supabase.auth.getSession();
+  //Info de usuario
+  const { data } = await authUser;
+
+  const liLogin = document.createElement("li");
+  //Si una sesion iniciada mostrara el boton de cerrar sesion
+  if (data.session) {
+    const name = data.session.user.user_metadata.name;
+    const logoutButton = document.createElement("button");
+    //El boton tendra el nombre del usuario
+    logoutButton.innerHTML = /* html */ `
+    Cerrar sesión
+    <span class='button__signOut-name'>(${name})</span>
+    `;
+    liLogin.appendChild(logoutButton);
+    navbarMenuContainer.appendChild(liLogin);
+    //Cerrar sesion y recargar la pagina
+    logoutButton.addEventListener("click", async () => {
+      await supabase.auth.signOut();
+      window.location.reload();
+    });
+    //Mostrar enlace para acceder
+  } else {
+    const aLogin = document.createElement("a");
+    aLogin.href = isPage ? url : `pages/login.html`;
+    aLogin.textContent = "acceder";
+    liLogin.appendChild(aLogin);
+    navbarMenuContainer.appendChild(liLogin);
+  }
+
   links.map(({ text, url }) => {
     const li = document.createElement("li");
     const a = document.createElement("a");
@@ -51,7 +80,7 @@ export const generateLinks = () => {
     navbarMenuContainer.appendChild(li);
   });
 
-  /* Agregar el index */
+  /* Agregar el Inicio */
   const li = document.createElement("li");
   const a = document.createElement("a");
   a.href = isPage ? "../index.html" : "./index.html";
