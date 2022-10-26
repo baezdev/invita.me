3d30830b7b88d9fc89caa4712e8407edcd451498
baezdev <baezdev@gmail.com>
2022-10-26 16:38:16 -0500

    docs🫥: funciones de logeo

--- js/auth/signIn.js
+++ js/auth/signIn.js
@@ -1,5 +1,13 @@
 import { supabase } from "../config/supabase.config.js";
 
+/**
+ * Toma un correo electrónico, una contraseña y un nombre, y luego usa la API de autenticación de Supabase para crear un nuevo usuario
+ * con esa información.
+ * @param email - La dirección de correo electrónico del usuario.
+ * @param password - string
+ * @param name - El nombre del usuario
+ * @returns La respuesta es un objeto
+ */
 export const registerNewUser = async (email, password, name) => {
   const response = await supabase.auth.signUp({
     email,
@@ -14,6 +22,12 @@ export const registerNewUser = async (email, password, name) => {
   return await response;
 };
 
+/**
+ * Toma un correo electrónico y una contraseña, y devuelve una respuesta de la API de Supabase.
+ * @param email - La dirección de correo electrónico del usuario.
+ * @param password - La contraseña del usuario.
+ * @returns La respuesta es un objeto
+ */
 export const loginWithEmail = async (email, password) => {
   const response = await supabase.auth.signInWithPassword({
     email,
